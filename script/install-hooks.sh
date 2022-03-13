74a4d5a83b5abc655af9bf2058f2c94017a2013c
Jose Goncalves <jose.goncalves@mindera.com>
2022-03-13 00:16:29 +0000

    git hooks

--- script/install-hooks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-GIT_DIR=$(git rev-parse --git-dir)
-
-echo "Installing hooks..."
-# this command creates symlink to our pre-commit script
-ln -s ../../scripts/pre-commit.bash $GIT_DIR/hooks/pre-commit
-echo "Done"!
